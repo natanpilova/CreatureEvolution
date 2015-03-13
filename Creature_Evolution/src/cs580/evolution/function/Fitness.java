55097e7d17430d2f1836b803ea71cfab5bdca232
Natalia Anpilova <natanpilova@gmail.com>
2015-03-13 16:59:44 -0400

    New package for functions: please add your function classes here! Stub for fitness function

---
+++ Creature_Evolution/src/cs580/evolution/function/Fitness.java
@@ -0,0 +1,25 @@
+/**
+ * Class for fitness function
+ */
+package cs580.evolution.function;
+
+import cs580.evolution.pojo.Environment;
+import cs580.evolution.pojo.Genome;
+
+/**
+ * @author Natalia Anpilova
+ *
+ */
+public class Fitness {
+
+	/**
+	 * fitness function
+	 * @param individual
+	 * @param currentEnvironment
+	 * @return fitness level of individual
+	 */
+	public int getFitness(Genome individual, Environment currentEnvironment) {
+		//TODO
+		return 100;
+	}
+}
