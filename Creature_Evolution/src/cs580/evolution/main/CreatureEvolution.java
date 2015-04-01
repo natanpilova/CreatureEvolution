e80846f6f49676287adc33389abead12b54d210a
Natalia Anpilova <natanpilova@gmail.com>
2015-04-01 00:00:24 -0400

    Added parents selection and nicer results output, comments

--- Creature_Evolution/src/cs580/evolution/main/CreatureEvolution.java
+++ Creature_Evolution/src/cs580/evolution/main/CreatureEvolution.java
@@ -12,6 +12,7 @@ import cs580.evolution.function.EnviGen;
 import cs580.evolution.function.Fitness;
 import cs580.evolution.function.Mutation;
 import cs580.evolution.function.Reproduce;
+import cs580.evolution.function.Selection;
 import cs580.evolution.pojo.Environment;
 import cs580.evolution.pojo.Genome;
 
@@ -34,7 +35,7 @@ public class CreatureEvolution {
 		/*
 		 * generating random initial population
 		 */
-		List<Genome> initPopulation = generateRandomPopulation(1000);	//arbitrary number of individuals
+		List<Genome> initPopulation = generateRandomPopulation(100);	//arbitrary number of individuals
 		//TODO after progress check: save randomly generated population to a file so it's possible to re-use it later
 		
 		//TODO after progress check: set environmental characteristics from input args
@@ -47,7 +48,7 @@ public class CreatureEvolution {
 		if (args.length > 0) {
 			int generationsNumber = Integer.parseInt(args[0]);
 			Entry<Integer, Genome> winner = geneticAlgoritm(initPopulation, initEnvironment, generationsNumber);
-			System.out.println("*********** WINNER ***********");	
+			System.out.println("\n*********** WINNER ***********");	
 			System.out.println("Individual with genome:");
 			System.out.println(winner.getValue().toString());
 			System.out.println("Fitness level: " + winner.getKey());
@@ -63,20 +64,24 @@ public class CreatureEvolution {
 	 * @return winner's genome
 	 */
 	public static Entry<Integer, Genome> geneticAlgoritm(List<Genome> initPopulation, Environment initEnvironment, int generationsNumber) {
-		List<Genome> population = initPopulation;	//current population, parents
+		List<Genome> population = initPopulation;	//current whole population
+		List<Genome> parentsPool = null;			//potential parents (population after discarding individuals with low fitness
+		List<Genome> parents = null;				//actual parents list
 		List<Genome> newPopulation = null;			//offspring
 		Genome mom, dad, child;
 		Environment environment = initEnvironment;
 		EnviGen envGenerator = new EnviGen();
 		Reproduce reproduce = new Reproduce();
 		Mutation mutation = new Mutation();
-		//TODO instantiate selection function
+		Selection selection = new Selection();
 		double tmp;	//to calculate log10 for generation number output
 		
-		//outer loop: one iteration = one generation
+		/*
+		 * outer loop: one iteration = one generation
+		 */
 		for (int generationCount = 1; generationCount <= generationsNumber; generationCount++) {
 			/*
-			 * Output generation number - log10 scale
+			 * output generation number - log10 scale
 			 */
 			tmp = Math.log10(generationCount);
 			if (tmp == (int)tmp || generationCount == generationsNumber)
@@ -85,26 +90,30 @@ public class CreatureEvolution {
 			newPopulation = new ArrayList<Genome>();
 			
 			/*
-			 * parents pre-selection
+			 * potential parents pre-selection based on the fitness level
 			 */
-			//TODO culling - discard individuals whose fitness is too low (define threshold in parent selector class)
-			//will be like: population = parentSelector.cull(population, environment), use fitness function fitness.getFitnessLevel() in cull()
+			parentsPool = selection.cullPopulation(population, environment);
 			
-			//inner loop: generating offspring as new population
+			/*
+			 * inner loop: generating offspring as new population
+			 */
 			//TODO maybe: instead of population size of the upper limit, use food limit from current environment state: if no more food left, no more offspring produced
 			for (int i = 0; i < population.size(); i++) {
 				/*
 				 * parents selection
 				 */
-				//TODO: both mom and dad will be selected like: mom = parentSelection.getParent(population, environment), use fitness function fitness.getFitnessLevel()
-				mom = population.get(0);
-				dad = population.get(1);
+				parents = selection.getParents(parentsPool, environment);
+				mom = parents.get(0);
+				dad = parents.get(1);
+				/*mom = population.get(0);
+				dad = population.get(1);*/
 				
 				/*
 				 * reproduce
 				 */
 				//TODO after progress check: adjust metabolism with weight, height and other genes. Also account for unlikely situations like height is 10cm and weight is 1 ton. -Natalia
 				child = reproduce.produceChild(mom, dad);
+				//TODO add offspring count back to corresponding genomes in parentsPool 
 				mom.addOffspring(1);
 				dad.addOffspring(1);
 				
@@ -118,7 +127,7 @@ public class CreatureEvolution {
 				newPopulation.add(child);
 			}
 			
-			//offspring will be parents in the next iteration
+			//offspring can be parents in the next iteration
 			population.clear();
 			population.addAll(newPopulation);
 			
@@ -127,6 +136,9 @@ public class CreatureEvolution {
 			 */
 			environment = envGenerator.EG(environment, generationCount);
 		}
+		
+		System.out.println("\nCurrent environment:");
+		System.out.println(environment.toString());
 		
 		//select individual with max fitness level
 		Entry<Integer, Genome> winner = getWinner(population, environment);
