d4eea1b0381a4ef72eec6175866254e8ece2d499
Natalia Anpilova <natanpilova@gmail.com>
2015-04-26 16:17:31 -0400

    Removed food surplus and offspring count from mutation

--- Creature_Evolution/src/cs580/evolution/function/Mutation.java
+++ Creature_Evolution/src/cs580/evolution/function/Mutation.java
@@ -2,6 +2,7 @@ package cs580.evolution.function;
 
 import java.util.Random;
 
+import cs580.evolution.pojo.AntForager;
 import cs580.evolution.pojo.Genome;
 
 
@@ -22,67 +23,41 @@ public class Mutation {
 		return randomNumber;	
 	}
 	
-	public static Genome mutate(Genome individual)
+	public static AntForager mutate(AntForager individual)
 	{
 		Random mutationGenerator = new Random();
-		int mutationSelector = mutationGenerator.nextInt(10);
+		int mutationSelector = mutationGenerator.nextInt(5);
 		GenomeEnum selectedType = GenomeEnum.values()[mutationSelector];
 		switch (selectedType) {
 		
-		case EYES :
-			int eyesNumber = individual.getEyesNumber();
-			eyesNumber = (eyesNumber + 1 > Genome.MAX_EYES_NUMBER) ? eyesNumber-1: eyesNumber+1;
-			individual.setEyesNumber(eyesNumber);
+		case SIZE :
+			double size = individual.getSize();
+			size = (size + 1 > AntForager.MAX_SIZE) ? size-1: size+1;
+			individual.setSize(size);
 			break;
-		case EARS :
-			int earsNumber= individual.getEarsNumber();
-			earsNumber = (earsNumber + 1 > Genome.MAX_EARS_NUMBER) ? earsNumber-1: earsNumber+1;
-			individual.setEarsNumber(earsNumber);
-			break;
-		case LEGS:
-			int legsNumber= individual.getLegsNumber();
-			legsNumber=(legsNumber + 1)>Genome.MAX_LEGS_NUMBER ? legsNumber-1 : legsNumber+1;
-			individual.setLegsNumber(legsNumber);
-			break;
-		case FINS:
-			int finsNumber= individual.getFinsNumber();
-			finsNumber=(finsNumber +1)> Genome.MAX_FINS_NUMBER?finsNumber-1: finsNumber+1;
-			individual.setFinsNumber(finsNumber);
-			break;
-		case WINGS:
-			int wingsNumber= individual.getFinsNumber();
-			wingsNumber=(wingsNumber+1)> Genome.MAX_WINGS_NUMBER ? wingsNumber-1: wingsNumber+1;
-			individual.setWingsNumber(wingsNumber);
-			break;
-		case THICKNESS:
-			int thickness= individual.getCoatThickness();
-			thickness=(thickness+10)> Genome.MAX_COAT_THICKNESS? thickness-1: thickness+1;
-			individual.setCoatThickness(thickness);
-			break;
-		case HEIGHT:
-			double height= individual.getHeight();
-			height=(height+10)> Genome.MAX_HEIGHT? height-10: height +10;
-			individual.setHeight((int) height);
-			break;
-		case WEIGHT:
-			int weight= individual.getWeight();
-			weight= (weight+10)> Genome.MAX_WEIGHT? weight-10 :weight+10;
+		case WEIGHT :
+			double weight= individual.getWeight();
+			weight = (weight + 1 > AntForager.MAX_WEIGHT) ? weight-1: weight+1;
 			individual.setWeight(weight);
 			break;
-		case METABOLISM:
-			int metabolism= individual.getMetabolism();
-			metabolism= (metabolism+100) > Genome.MAX_METABOLISM? metabolism-100: metabolism+100;
-			individual.setMetabolism(metabolism);
+		case JAWSIZE:
+			double jawsize= individual.getJawSize();
+			jawsize=(jawsize + 0.2)>AntForager.MAX_JAW_SIZE ? jawsize-0.2 : jawsize+0.2;
+			individual.setJawSize(jawsize);
 			break;
-		case COOPERATIONFLAG:
-			Random random = new Random();
-		    boolean cooperationFlag_random=random.nextBoolean();
-		    individual.setCooperationFlag(cooperationFlag_random);
-		    break;
-		
+		case FERONOMEINTENSITY:
+			double feronomeintensity= individual.getFeromoneIntensity();
+			feronomeintensity=(feronomeintensity +10)> AntForager.MAX_FEROMONE_INTENSITY?feronomeintensity-10: feronomeintensity+10;
+			individual.setFeromoneIntensity(feronomeintensity);
+			break;
+		case ANTENNAESENSORS:
+			int antennaeSensors= individual.getAntennaeSensors();
+			antennaeSensors=(antennaeSensors+100)> AntForager.MAX_SENSORS ? antennaeSensors-100: antennaeSensors+100;
+			individual.setAntennaeSensors(antennaeSensors);
+			break;
 		}	
-		return individual;
 		
+		return individual;
 	}
 
 }
