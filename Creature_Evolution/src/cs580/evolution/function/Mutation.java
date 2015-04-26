c05babb9a9254a0fd07851ef7849a183e77f59c7
Natalia Anpilova <natanpilova@gmail.com>
2015-04-26 16:18:51 -0400

    Deleted not needed comment

--- Creature_Evolution/src/cs580/evolution/function/Mutation.java
+++ Creature_Evolution/src/cs580/evolution/function/Mutation.java
@@ -11,10 +11,6 @@ import cs580.evolution.pojo.Genome;
  *
  */
 public class Mutation {
-	//TODO (optional - Divya): add some additional small value if current environment's pollution exceeds mutation threshold, like:
-	//if (env.getPollutionLevel => Envoronment.POLLUTION_MUTATION_THRESHOLD {randomNumber += env.getPollutionLevel/100}
-	//Remember that the result should be less than 1
-	//Also, mutationProbability() will need to have current Environment as an argument
 	
 	public double mutationProbability()
 	{
